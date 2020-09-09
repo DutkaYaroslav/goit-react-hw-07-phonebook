9811ad1379fafeceb9fe31bb0226f5d4e4148c10
unknown <yaroslavdutka@gmail.com>
2020-09-09 17:57:44 +0300

    Updates

---
+++ precache-manifest.93f0a26d152af740ac8ba1d2ed176e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15944e50200129ac0b30224d5906aaac",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "7e5745edc692619d9fa5",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.7ff123cb.chunk.css"
+  },
+  {
+    "revision": "e310e1c65d5886574608",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.4e15f819.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.4e15f819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5745edc692619d9fa5",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.a6134be7.chunk.js"
+  },
+  {
+    "revision": "0918efebc45e61204358",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.645d6657.js"
+  }
+]);
